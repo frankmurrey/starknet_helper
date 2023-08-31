961c2f6a92c55d62a9f9bc6a13d5ebcc3d9f729d
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 00:07:49 +0500

    ADD 10kswap

--- contracts/k10swap/main.py
+++ contracts/k10swap/main.py
@@ -2,7 +2,7 @@ from src.paths import K10SwapDir
 from utlis.file_manager import FileManager
 
 
-class JediSwapContracts:
+class K10SwapContracts:
     def __init__(self):
-        self.router_address = '0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023'
+        self.router_address = '0x07a6f98c03379b9513ca84cca1373ff452a7462a3b61598f0af5bb27ad7f76d1'
         self.router_abi = FileManager.read_abi_from_file(K10SwapDir.ROUTER_ABI_FILE)

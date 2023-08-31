3c4594eec201bcdcce8da59b41afa1d9ebd88da6
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 00:16:31 +0500

    UPD base class

--- src/schemas/configs/jediswap.py
+++ src/schemas/configs/jediswap.py
@@ -1,17 +1,9 @@
 from typing import Union
+from src.schemas.configs.base import SwapSettingsBase
 
-from src.schemas.configs.base import CommonSettingsBase
 from src import enums
 
 
-class JediSwapConfigSchema(CommonSettingsBase):
-    module_name: str = "jediswap"
+class JediSwapConfigSchema(SwapSettingsBase):
+    module_name: str = 'jediswap_swap'
     module_type: enums.ModuleType = enums.ModuleType.SWAP
-
-    coin_to_swap: Union[str, None] = ""
-    coin_to_receive: Union[str, None] = ""
-    min_amount_out: Union[int, float, str] = 0
-    max_amount_out: Union[int, float, str] = 0
-    use_all_balance: Union[bool, str] = False
-    send_percent_balance: Union[bool, str] = False
-    slippage: Union[int, float, str] = 0

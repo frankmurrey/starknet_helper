a03fbf3df291168508cfb66bcbe83f0eb0412e81
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 23:37:51 +0500

    UPD

--- src/schemas/configs/myswap.py
+++ src/schemas/configs/myswap.py
@@ -1,5 +1,8 @@
 from src.schemas.configs.base import SwapSettingsBase
 
+from src import enums
 
-class MySwapConfigSchema( SwapSettingsBase):
-    module_name: str = 'myswap_swap'
+
+class MySwapConfigSchema(SwapSettingsBase):
+    module_name: enums.ModuleName = enums.ModuleName.MY_SWAP
+    module_type: enums.ModuleType = enums.ModuleType.SWAP

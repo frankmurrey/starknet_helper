8d3007f5693edc3dfb0da79cb9aa56b121541724
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 23:37:21 +0500

    UPD

--- src/schemas/configs/dmail.py
+++ src/schemas/configs/dmail.py
@@ -1,7 +1,10 @@
 from src.schemas.configs.base import CommonSettingsBase
 
+from src import enums
+
 
 class DmailSendMailConfigSchema(CommonSettingsBase):
-    module_name: str = 'dmail_send_mail'
+    module_name: enums.ModuleName = enums.ModuleName.DMAIL
+    module_type: enums.ModuleType = enums.ModuleType.SEND_MAIL
     custom_mails: bool = False
     custom_messages: bool = False

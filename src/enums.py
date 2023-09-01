faa7ad66defcfe5da53a420ca2c49978c7f82cdd
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 23:35:37 +0500

    UPD

--- src/enums.py
+++ src/enums.py
@@ -3,15 +3,25 @@ from enum import Enum
 
 class ModuleType(Enum):
     SWAP = "swap"
+    MINT = 'mint'
+    SUPPLY = 'supply'
+    BORROW = 'borrow'
+    WITHDRAW = 'withdraw'
     LIQUIDITY_ADD = "liquidity_add"
     LIQUIDITY_REMOVE = "liquidity_remove"
+    SEND_MAIL = 'send_mail'
 
 
 class ModuleName(str, Enum):
     JEDI_SWAP = "jediswap"
     MY_SWAP = "myswap"
-    DEPLOY_ARGENT = "deploy_argent"
-    DEPLOY_BRAAVOS = "deploy_braavos"
+    DEPLOY = "deploy"
+    DMAIL = 'dmail'
+    IDENTITY = 'identity'
+    K10SWAP = 'k10swap'
+    SITHSWAP = 'sithswap'
+    AVNU = 'avnu'
+    ZKLEND = 'zklend'
 
 
 class PrivateKeyType(Enum):

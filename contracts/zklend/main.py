04736741e70fa062de5431436c0f5d1ba0439707
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-01 23:33:36 +0500

    ADD zklend

---
+++ contracts/zklend/main.py
@@ -0,0 +1,8 @@
+from src.paths import ZkLendDir
+from utlis.file_manager import FileManager
+
+
+class ZkLendContracts:
+    def __init__(self):
+        self.router_address = '0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05'
+        self.router_abi = FileManager.read_abi_from_file(ZkLendDir.ROUTER_ABI_FILE)

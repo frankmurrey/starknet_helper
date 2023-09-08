bceed22baecdf14c031ca73ad1a5b632663cc876
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-08 19:31:16 +0500

    UPD

--- src/schemas/configs/app_config.py
+++ src/schemas/configs/app_config.py
@@ -10,6 +10,9 @@ class AppConfigSchema(BaseModel):
     mobile_proxy_rotation: bool = False
     mobile_proxy_rotation_link: Union[str, None] = ""
     rpc_url: str = "https://starknet-mainnet.public.blastapi.io"
+    eth_mainnet_rpc_url: str = "https://rpc.ankr.com/eth"
+    target_eth_mainnet_gas_price: Union[int, float] = 20
+    time_to_wait_target_gas_price_sec: Union[int, float] = 360
 
     shuffle_wallets: bool = False
     private_key_type: PrivateKeyType = PrivateKeyType.argent

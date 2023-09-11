483962a6bd072a9f34a27fe48682d49e6bafe110
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-11 20:16:23 +0500

    UPD grids

--- gui/wallet_window/wallet_item.py
+++ gui/wallet_window/wallet_item.py
@@ -20,10 +20,11 @@ class WalletItem(customtkinter.CTkFrame):
         self.frame = customtkinter.CTkFrame(master)
         self.frame.grid(**grid)
 
-        self.frame.grid_columnconfigure((0, 1, 2, 3), weight=1, uniform="a")
+        self.frame.grid_columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="uniform")
+
         self.frame.grid_rowconfigure(0, weight=1)
 
-        pad_x = 50
+        pad_x = (40, 0)
         kf = 0
 
         pad_y = 5
@@ -39,7 +40,7 @@ class WalletItem(customtkinter.CTkFrame):
         self.chose_checkbox.grid(
             row=0,
             column=0,
-            padx=10,
+            padx=(10, 0),
             pady=pad_y,
             sticky="w",
         )
@@ -52,43 +53,37 @@ class WalletItem(customtkinter.CTkFrame):
         self.wallet_name_label.grid(
             row=0,
             column=0,
-            padx=pad_x,
+            padx=(50, 0),
             pady=pad_y,
             sticky="w"
         )
 
-        pad_x = pad_x + kf
-
         self.wallet_address_label = customtkinter.CTkLabel(
             self.frame,
-            text=wallet_data.private_key,
+            text=self.get_short_address(wallet_data.private_key),
             font=customtkinter.CTkFont(size=12, weight="bold"),
             anchor="w"
         )
         self.wallet_address_label.grid(
             row=0,
             column=1,
-            padx=(0, pad_x * 2),
+            padx=0,
             pady=pad_y,
         )
 
-        pad_x = pad_x + kf
-
-        proxy = f"{wallet_data.proxy.host}:{wallet_data.proxy.port}" if wallet_data.proxy else "-"
         self.proxy_address_label = customtkinter.CTkLabel(
             self.frame,
-            text=proxy,
+            text=self.get_short_proxy(host='http://192.168.321.23.1',
+                                      port='12345'),
             font=customtkinter.CTkFont(size=12, weight="bold")
         )
         self.proxy_address_label.grid(
             row=0,
             column=2,
-            padx=pad_x,
+            padx=(30, 0),
             pady=pad_y,
             sticky="w"
         )
-
-        pad_x = pad_x + kf
 
         self.wallet_type_label = customtkinter.CTkLabel(
             self.frame,
@@ -98,12 +93,10 @@ class WalletItem(customtkinter.CTkFrame):
         self.wallet_type_label.grid(
             row=0,
             column=3,
-            padx=pad_x,
+            padx=(100, 0),
             pady=pad_y,
             sticky="w"
         )
-
-        pad_x = pad_x + kf
 
         self.edit_button = customtkinter.CTkButton(
             self.frame,
@@ -114,8 +107,8 @@ class WalletItem(customtkinter.CTkFrame):
         )
         self.edit_button.grid(
             row=0,
-            column=3,
-            padx=pad_x - 25,
+            column=4,
+            padx=(0, 80),
             pady=pad_y,
             sticky="e"
         )
@@ -123,3 +116,22 @@ class WalletItem(customtkinter.CTkFrame):
     @property
     def is_chosen(self):
         return self.chose_checkbox.get()
+
+    @staticmethod
+    def get_short_address(
+            address: str
+    ):
+        return address[:6] + "..." + address[-4:]
+
+    @staticmethod
+    def get_short_proxy(
+            host: str,
+            port: str
+    ):
+        if "//" in host:
+            host = host.split("//")[1]
+
+        # proxy = host[:8] + " " + ":" + port
+        proxy = host + port
+
+        return proxy

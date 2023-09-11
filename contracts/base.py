95853129be3e07a25f43c8c06abd5cafbf4dd5d4
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-11 20:05:37 +0500

    ADD available_protocols field

--- contracts/base.py
+++ contracts/base.py
@@ -4,15 +4,15 @@ class TokenBase:
     def __init__(self,
                  symbol: str,
                  contract_address: str,
-                 is_jedi_available: bool = None,
-                 is_my_swap_available: bool = None,
-                 is_zklend_available: bool = None,
                  coin_gecko_id: str = None,
+                 available_protocols: list = None,
                  abi: str = None):
+        if available_protocols is None:
+            available_protocols = []
+
         self.symbol = symbol
+        self.available_protocol = available_protocols
         self.contract_address = contract_address
         self.abi = abi
-        self.is_jedi_available = is_jedi_available
-        self.is_my_swap_available = is_my_swap_available
-        self.is_zklend_available = is_zklend_available
+
         self.coin_gecko_id = coin_gecko_id

2b2c719dcffa5b1e755e223c910d3e33191880c3
preposition17 <yupich17@gmail.com>
2023-09-12 23:03:15 +0500

    ADD CTkEntryWithLabel component

---
+++ gui/objects.py
@@ -0,0 +1,25 @@
+import tkinter
+from typing import Optional, Union, Tuple
+
+import customtkinter
+
+
+class CTkEntryWithLabel(customtkinter.CTkFrame):
+    """
+    Entry with a label
+    """
+
+    def __init__(self, master, label_text: str, state: str = tkinter.NORMAL, **kwargs):
+        super().__init__(master, fg_color="transparent", **kwargs)
+
+        self.label = customtkinter.CTkLabel(self, text=label_text)
+        self.label.grid(row=0, column=0, padx=0, pady=0, sticky="w")
+
+        self.entry = customtkinter.CTkEntry(self, state=state, **kwargs)
+        self.entry.grid(row=1, column=0, padx=0, pady=0, sticky="w")
+
+    def get(self):
+        return self.entry.get()
+
+    def bind(self, sequence, command, add=True):
+        self.entry.bind(sequence, command, add)

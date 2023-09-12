da356092ec9786d56eb1b721872284896c5b79d9
preposition17 <yupich17@gmail.com>
2023-09-12 23:06:23 +0500

    CLR

--- gui/main_window/wallet_action_frame.py
+++ gui/main_window/wallet_action_frame.py
@@ -1,16 +1,12 @@
 import customtkinter
 
-from src.schemas.configs.transaction_settings_base.swap import SwapSettingsBase
+from src.schemas.tasks.base.swap import SwapTaskBase
 
 
 class WalletActionFrame(customtkinter.CTkFrame):
     def __init__(
-            self,
-            master: any,
-            grid: dict,
-            config: SwapSettingsBase,
-            repeats: int,
-            **kwargs):
+        self, master: any, grid: dict, task: SwapTaskBase, repeats: int, **kwargs
+    ):
         super().__init__(master, **kwargs)
 
         self.grid(**grid)
@@ -19,39 +15,19 @@ class WalletActionFrame(customtkinter.CTkFrame):
 
         self.module_label = customtkinter.CTkLabel(
             self,
-            text=config.module_name.title(),
-            font=customtkinter.CTkFont(size=12, weight="bold")
+            text=task.module_name.title(),
+            font=customtkinter.CTkFont(size=12, weight="bold"),
         )
-        self.module_label.grid(
-            row=0,
-            column=0,
-            padx=(10, 0),
-            pady=0
-        )
+        self.module_label.grid(row=0, column=0, padx=(10, 0), pady=0)
 
         self.action_label = customtkinter.CTkLabel(
             self,
-            text=config.module_type.title(),
-            font=customtkinter.CTkFont(size=12, weight="bold")
+            text=task.module_type.title(),
+            font=customtkinter.CTkFont(size=12, weight="bold"),
         )
-        self.action_label.grid(
-            row=0,
-            column=1,
-            padx=(15, 0),
-            pady=0
-        )
+        self.action_label.grid(row=0, column=1, padx=(15, 0), pady=0)
 
         self.repeats_label = customtkinter.CTkLabel(
-            self,
-            text=str(repeats),
-            font=customtkinter.CTkFont(size=12, weight="bold")
+            self, text=str(repeats), font=customtkinter.CTkFont(size=12, weight="bold")
         )
-        self.repeats_label.grid(
-            row=0,
-            column=2,
-            padx=(0, 5),
-            pady=0
-        )
-
-
-
+        self.repeats_label.grid(row=0, column=2, padx=(0, 5), pady=0)

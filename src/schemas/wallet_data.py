3e082f1346275dffb5b8978672b0534031a0b70d
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-13 13:15:49 +0500

    ADD address property

--- src/schemas/wallet_data.py
+++ src/schemas/wallet_data.py
@@ -5,9 +5,11 @@ from pydantic import validator
 from loguru import logger
 
 from src import enums
-from src.schemas.proxy_data import ProxyData
-from utlis.proxy import parse_proxy_data
 from src import exceptions
+from src.schemas.proxy_data import ProxyData
+from utlis.key_manager.key_manager import get_argent_addr_from_private_key
+from utlis.key_manager.key_manager import get_braavos_addr_from_private_key
+from utlis.proxy import parse_proxy_data
 
 
 class WalletData(BaseModel):
@@ -18,12 +20,14 @@ class WalletData(BaseModel):
     type: enums.PrivateKeyType = enums.PrivateKeyType.argent
 
     @validator("proxy", pre=True)
+    @classmethod
     def validate_proxy(cls, v):
         if isinstance(v, str):
             return parse_proxy_data(proxy_str=v)
         return v
 
     @validator("type", pre=True)
+    @classmethod
     def validate_type(cls, v):
         if isinstance(v, str):
             try:
@@ -32,6 +36,15 @@ class WalletData(BaseModel):
                 logger.error("Bad private key type")
 
         return v
+
+    @property
+    def address(self) -> str:
+        if self.type == enums.PrivateKeyType.argent:
+            return hex(get_argent_addr_from_private_key(private_key=self.private_key))
+        elif self.type == enums.PrivateKeyType.braavos:
+            return hex(get_braavos_addr_from_private_key(private_key=self.private_key))
+        else:
+            raise exceptions.AppValidationError(f"Bad private key type: {self.type}")
 
     @validator("private_key", pre=True)
     def validate_private_key(cls, v):

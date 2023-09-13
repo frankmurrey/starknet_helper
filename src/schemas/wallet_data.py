258e4c1546bb3182cd534db3cfbb14537f7cf7bd
preposition17 <yupich17@gmail.com>
2023-09-14 01:14:08 +0500

    CLR

--- src/schemas/wallet_data.py
+++ src/schemas/wallet_data.py
@@ -1,6 +1,7 @@
 from typing import Union, Optional
 
 from pydantic import BaseModel
+from pydantic import Field
 from pydantic import validator
 from loguru import logger
 
@@ -15,25 +16,30 @@ from utlis.proxy import parse_proxy_data
 class WalletData(BaseModel):
     name: Optional[str] = None
     private_key: str
+    pair_address: str
     proxy: Optional[ProxyData] = None
-    pair_address: Optional[str] = None
     type: enums.PrivateKeyType = enums.PrivateKeyType.argent
 
     @validator("proxy", pre=True)
-    @classmethod
     def validate_proxy(cls, v):
         if isinstance(v, str):
             return parse_proxy_data(proxy_str=v)
         return v
 
     @validator("type", pre=True)
-    @classmethod
     def validate_type(cls, v):
         if isinstance(v, str):
             try:
                 return enums.PrivateKeyType[v.lower()]
             except KeyError:
                 logger.error("Bad private key type")
+
+        return v
+
+    @validator("private_key", pre=True)
+    def validate_private_key(cls, v):
+        if not v:
+            raise exceptions.AppValidationError("Private key is required")
 
         return v
 
@@ -45,10 +51,3 @@ class WalletData(BaseModel):
             return hex(get_braavos_addr_from_private_key(private_key=self.private_key))
         else:
             raise exceptions.AppValidationError(f"Bad private key type: {self.type}")
-
-    @validator("private_key", pre=True)
-    def validate_private_key(cls, v):
-        if not v:
-            raise exceptions.AppValidationError("Private key is required")
-
-        return v

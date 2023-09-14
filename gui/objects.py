fc3226fc140ffe9a7a1777d570d8d24988b9b1fa
preposition17 <yupich17@gmail.com>
2023-09-15 00:57:59 +0500

    ADD width and height patameters

--- gui/objects.py
+++ gui/objects.py
@@ -13,19 +13,29 @@ class CTkEntryWithLabel(customtkinter.CTkFrame):
             self,
             master,
             label_text: str,
+
             textvariable: Union[tkinter.Variable, None] = None,
+
+            width: int = 140,
+            height: int = 28,
+
             state: str = tkinter.NORMAL,
             **kwargs
     ):
         super().__init__(master, fg_color="transparent", **kwargs)
 
-        self.label = customtkinter.CTkLabel(self, text=label_text)
+        self.label = customtkinter.CTkLabel(
+            self,
+            text=label_text,
+        )
         self.label.grid(row=0, column=0, padx=0, pady=0, sticky="w")
 
         self.entry = customtkinter.CTkEntry(
             self,
             state=state,
             textvariable=textvariable,
+            width=width,
+            height=height,
             **kwargs
         )
         self.entry.grid(row=1, column=0, padx=0, pady=0, sticky="w")

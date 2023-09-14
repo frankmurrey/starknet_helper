a8f2dfbe6c14421059d98dd18f4c9a7d8c4a15df
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-14 23:43:09 +0500

    UPD imports

--- gui/wallet_right_window/wallets_table.py
+++ gui/wallet_right_window/wallets_table.py
@@ -3,8 +3,8 @@ from typing import List
 
 import customtkinter
 
-from gui.wallet_window.wallet_item import WalletItem
-from gui.wallet_window.frames import WalletsTableTop
+from gui.wallet_right_window.wallet_item import WalletItem
+from gui.wallet_right_window.frames import WalletsTableTop
 from src.schemas.proxy_data import ProxyData
 from src.schemas.wallet_data import WalletData
 

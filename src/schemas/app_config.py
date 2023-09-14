a25bfbc27f193e2a4cf5ef89c56f978cb2f668dd
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-14 23:43:38 +0500

    ADD fields validations

--- src/schemas/app_config.py
+++ src/schemas/app_config.py
@@ -1,18 +1,48 @@
 from typing import Union
 
 from pydantic import BaseModel
-
-from src.enums import PrivateKeyType
+from pydantic import validator
+from src import exceptions
+from utlis import validation
 
 
 class AppConfigSchema(BaseModel):
     preserve_logs: bool = True
-    mobile_proxy_rotation: bool = False
-    mobile_proxy_rotation_link: Union[str, None] = ""
     rpc_url: str = "https://starknet-mainnet.public.blastapi.io"
     eth_mainnet_rpc_url: str = "https://rpc.ankr.com/eth"
     target_eth_mainnet_gas_price: Union[int, float] = 20
     time_to_wait_target_gas_price_sec: Union[int, float] = 360
 
-    shuffle_wallets: bool = False
-    private_key_type: PrivateKeyType = PrivateKeyType.argent
+    @validator('rpc_url', pre=True)
+    def rpc_url_must_be_valid(cls, value):
+        if not value:
+            raise exceptions.AppValidationError("RPC URL can't be empty")
+
+        return value
+
+    @validator('eth_mainnet_rpc_url', pre=True)
+    def eth_mainnet_rpc_url_must_be_valid(cls, value):
+        if not value:
+            raise exceptions.AppValidationError("ETH Mainnet RPC URL can't be empty")
+
+        return value
+
+    @validator('target_eth_mainnet_gas_price', pre=True)
+    def target_eth_mainnet_gas_price_must_be_valid(cls, value):
+        value = validation.get_converted_to_int(value, "Gas Price")
+        value = validation.get_positive(value, "Gas Price", include_zero=False)
+
+        return value
+
+    @validator('time_to_wait_target_gas_price_sec', pre=True)
+    def time_to_wait_target_gas_price_sec_must_be_valid(cls, value):
+        value = validation.get_converted_to_int(value, "Time to wait")
+        value = validation.get_positive(value, "Time to wait", include_zero=True)
+
+        return value
+
+
+
+
+
+

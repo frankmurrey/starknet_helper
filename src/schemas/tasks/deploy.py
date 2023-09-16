e011a8bde2800828867bce2876d61cf972fbc426
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-16 19:05:57 +0500

    ADD UpgradeTask

--- src/schemas/tasks/deploy.py
+++ src/schemas/tasks/deploy.py
@@ -4,6 +4,7 @@ from pydantic import Field
 
 from src.schemas.tasks.base import TaskBase
 from modules.deploy.deploy import Deploy
+from modules.deploy.upgrade import Upgrade
 from src import enums
 
 
@@ -11,3 +12,9 @@ class DeployTask(TaskBase):
     module_name: enums.ModuleName = enums.ModuleName.DEPLOY
     module_type: enums.ModuleType = ""
     module: Callable = Field(default=Deploy)
+
+
+class UpgradeTask(TaskBase):
+    module_name: enums.ModuleName = enums.ModuleName.UPGRADE
+    module_type: enums.ModuleType = ""
+    module: Callable = Field(default=Upgrade)

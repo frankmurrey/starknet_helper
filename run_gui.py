e3453589a49ae9089efd89fc684369220c3e0c55
preposition17 <yupich17@gmail.com>
2023-09-21 00:00:29 +0500

    CLR

--- run_gui.py
+++ run_gui.py
@@ -1,22 +1,5 @@
-from src.tasks_executor import TasksExecutor
 from gui.main_window.main import run_gui
 
 
-def on_startup():
-    pass
-#     Templates().create_not_found_temp_files()
-    #
-    # if not mingw_installed():
-    #     logger.critical("MinGW in not installed.")  # TODO: More info
-    #     exit(1)
-
-
 if __name__ == '__main__':
-    task_executor = TasksExecutor()
-
-    on_startup()
-
-    try:
-        run_gui()
-    except KeyboardInterrupt:
-        task_executor.stop()
+    run_gui()

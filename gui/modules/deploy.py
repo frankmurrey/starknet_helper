a4ddbdc645d2cf509e237e155705a661207536b6
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-20 17:01:06 +0500

    ADD info_textbox

--- gui/modules/deploy.py
+++ gui/modules/deploy.py
@@ -1,6 +1,9 @@
+import customtkinter
+
 from src.schemas.tasks.deploy import DeployTask
 
 from gui.modules.txn_settings_frame import TxnSettingFrame
+from gui.objects import CTkCustomTextBox
 
 
 class DeployTab:
@@ -11,7 +14,7 @@ class DeployTab:
     ):
         self.tabview = tabview
 
-        self.tab_name = tab_name
+        self.tabview.tab(tab_name).grid_columnconfigure(0, weight=1)
 
         self.txn_settings_frame = TxnSettingFrame(
             master=self.tabview.tab(tab_name),
@@ -22,6 +25,23 @@ class DeployTab:
                 "pady": 20,
                 "sticky": "nsew"
             }
+        )
+
+        text_box_grid = {
+            "row": 1,
+            "column": 0,
+            "padx": 20,
+            "pady": 20,
+            "sticky": "ew"
+        }
+
+        text = f"- Wallet type must be specified for each wallet.\n\n" \
+               f"- New wallets will be deployed as Cairo 1"
+
+        self.info_textbox = CTkCustomTextBox(
+            master=self.tabview.tab(tab_name),
+            grid=text_box_grid,
+            text=text,
         )
 
     def build_config_data(self):

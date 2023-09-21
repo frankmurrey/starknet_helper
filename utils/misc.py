0bedd50bfe674fd9eddf0d45a45e8eb98e47cacb
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-21 20:45:26 +0500

    RENAME

---
+++ utils/misc.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+
+def mingw_installed() -> bool:
+    try:
+
+        exec_result = (
+            subprocess.check_output(["gcc", "--version"])
+            .decode("utf-8")
+            .lower()
+        )
+
+        return "mingw" in exec_result
+
+    except FileNotFoundError:
+        return False
+
+
+if __name__ == '__main__':
+    print(mingw_installed())

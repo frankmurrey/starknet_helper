8f828d1362c0c03582c84c299db25dc7950e6b4d
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-22 20:15:30 +0500

    ADD logger configuration

--- src/logger.py
+++ src/logger.py
@@ -3,8 +3,8 @@ from sys import stderr
 
 
 def configure_logger():
-    # logger.remove()
-    # logger.add(stderr,
-    #            format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{line: <3}</cyan>"
-    #                   " - <level>{message}</level>", level='INFO')
+    logger.remove()
+    logger.add(stderr,
+               format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{line: <3}</cyan>"
+                      " - <level>{message}</level>", level='INFO')
     pass

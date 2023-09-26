1dea36ec007d8ecc526bac7f5096e05f18bab1ab
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-26 11:28:01 +0500

    ADD decode_wallet_version

--- utils/misc.py
+++ utils/misc.py
@@ -17,5 +17,26 @@ def mingw_installed() -> bool:
         return False
 
 
+def decode_wallet_version(version: int) -> str:
+    """
+    Converts a binary version number to a human-readable string.
+
+    Args:
+    version (int): The version number to be converted.
+
+    Returns:
+    str: The human-readable version number.
+    """
+    version_bytes = version.to_bytes(31, byteorder="big")
+
+    char_list = [chr(i) for i in version_bytes]
+
+    version_string = "".join(char_list)
+
+    final_version = version_string.lstrip("\x00")
+
+    return final_version
+
+
 if __name__ == '__main__':
     print(mingw_installed())

2f75b1402545ac572c18cfacad47afc0a9086e75
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-09-30 15:46:01 +0500

    CLR

--- utils/xlsx.py
+++ utils/xlsx.py
@@ -3,31 +3,6 @@ import pandas as pd
 from loguru import logger
 
 
-def write_balance_data_to_xlsx(path,
-                               data: list[dict],
-                               coin_option):
-
-    datapd = {
-        "Wallet Address": [],
-        "Balance": [],
-        "Coin": []
-    }
-
-    for wallet in data:
-        for addr, balance in wallet.items():
-            datapd["Wallet Address"].append(addr)
-            datapd["Balance"].append(balance)
-            datapd["Coin"].append(coin_option)
-
-    df = pd.DataFrame(datapd)
-    try:
-        df.to_excel(path, index=False)
-
-        logger.warning(f"Balance data saved to {path}")
-    except Exception as e:
-        logger.error(f"Error while saving balance data to {path}: {e}")
-
-
 def write_wallet_action_to_xlsx():
     action_storage = ActionStorage()
     action = action_storage.get_current_action()

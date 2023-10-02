84345c02baa59fff8d02986469f211d926c38d31
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-02 18:23:57 +0500

    ADD mingw_installed check

--- run_gui.py
+++ run_gui.py
@@ -1,5 +1,12 @@
 from gui.main_window.main import run_gui
+from utils.misc import mingw_installed
+from loguru import logger
 
 
 if __name__ == '__main__':
+    if mingw_installed() is False:
+        logger.error("MinGW is not installed, please install it and try again")
+        logger.error("https://starknetpy.readthedocs.io/en/latest/installation.html#windows")
+        exit(1)
+
     run_gui()

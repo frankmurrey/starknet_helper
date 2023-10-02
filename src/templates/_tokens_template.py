135f3e4c2e5487ac46be47a21f8d7509cc055675
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-02 18:29:38 +0500

    UPD

--- src/templates/_tokens_template.py
+++ src/templates/_tokens_template.py
@@ -1,66 +1,90 @@
 TOKENS_DATA = [
-  {
-    "symbol": "eth",
-    "contract_address": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-    "is_jedi_available": True,
-    "is_my_swap_available": True,
-    "coin_gecko_id": "ethereum"
-  },
-  {
-    "symbol": "usdt",
-    "contract_address": "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
-    "is_jedi_available": True,
-    "is_my_swap_available": True,
-    "coin_gecko_id": "tether"
-  },
-  {
-    "symbol": "usdc",
-    "contract_address": "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
-    "is_jedi_available": True,
-    "is_my_swap_available": True,
-    "coin_gecko_id": "usd-coin"
-  },
-  {
-    "symbol": "dai",
-    "contract_address": "0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3",
-    "is_jedi_available": True,
-    "is_my_swap_available": True,
-    "coin_gecko_id": "dai"
-  },
-  {
-    "symbol": "zusdc",
-    "contract_address": "0x047ad51726d891f972e74e4ad858a261b43869f7126ce7436ee0b2529a98f486"
-  },
-  {
-    "symbol": "zusdt",
-    "contract_address": "0x00811d8da5dc8a2206ea7fd0b28627c2d77280a515126e62baa4d78e22714c4a"
-  },
-  {
-    "symbol": "zdai",
-    "contract_address": "0x062fa7afe1ca2992f8d8015385a279f49fad36299754fb1e9866f4f052289376"
-  },
-  {
-    "symbol": "zwbtc",
-    "contract_address": "0x02b9ea3acdb23da566cee8e8beae3125a1458e720dea68c4a9a7a2d8eb5bbb4a"
-  },
-  {
-    "symbol": "zeth",
-    "contract_address": "0x01b5bd713e72fdc5d63ffd83762f81297f6175a5e0a4771cdadbc1dd5fe72cb1"
-  },
-  {
-    "symbol": "msw_usdc_usdt",
-    "contract_address": "0x01ea237607b7d9d2e9997aa373795929807552503683e35d8739f4dc46652de1"
-  },
-  {
-    "symbol": "msw_dai_eth",
-    "contract_address": "0x07c662b10f409d7a0a69c8da79b397fd91187ca5f6230ed30effef2dceddc5b3"
-  },
-  {
-    "symbol": "msw_eth_usdc",
-    "contract_address": "0x022b05f9396d2c48183f6deaf138a57522bcc8b35b67dee919f76403d1783136"
-  },
-  {
-    "symbol": "msw_eth_usdt",
-    "contract_address": "0x041f9a1e9a4d924273f5a5c0c138d52d66d2e6a8bee17412c6b0f48fe059ae04"
-  }
+    {
+        "symbol": "eth",
+        "contract_address": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+        "available_protocols": [
+            "jediswap",
+            "myswap",
+            "sithswap",
+            "avnu",
+            "k10swap",
+            "zklend"
+        ],
+        "coin_gecko_id": "ethereum"
+    },
+    {
+        "symbol": "usdt",
+        "contract_address": "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
+        "available_protocols": [
+            "jediswap",
+            "myswap",
+            "sithswap",
+            "avnu",
+            "k10swap",
+            "zklend"
+        ],
+        "coin_gecko_id": "tether"
+    },
+    {
+        "symbol": "usdc",
+        "contract_address": "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+        "available_protocols": [
+            "jediswap",
+            "myswap",
+            "sithswap",
+            "avnu",
+            "k10swap",
+            "zklend"
+        ],
+        "coin_gecko_id": "usd-coin"
+    },
+    {
+        "symbol": "dai",
+        "contract_address": "0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3",
+        "available_protocols": [
+            "jediswap",
+            "myswap",
+            "sithswap",
+            "avnu",
+            "k10swap",
+            "zklend"
+        ],
+        "coin_gecko_id": "dai"
+    },
+    {
+        "symbol": "zusdc",
+        "contract_address": "0x047ad51726d891f972e74e4ad858a261b43869f7126ce7436ee0b2529a98f486"
+    },
+    {
+        "symbol": "zusdt",
+        "contract_address": "0x00811d8da5dc8a2206ea7fd0b28627c2d77280a515126e62baa4d78e22714c4a"
+    },
+    {
+        "symbol": "zdai",
+        "contract_address": "0x062fa7afe1ca2992f8d8015385a279f49fad36299754fb1e9866f4f052289376"
+    },
+    {
+        "symbol": "zwbtc",
+        "contract_address": "0x02b9ea3acdb23da566cee8e8beae3125a1458e720dea68c4a9a7a2d8eb5bbb4a"
+    },
+    {
+        "symbol": "zeth",
+        "contract_address": "0x01b5bd713e72fdc5d63ffd83762f81297f6175a5e0a4771cdadbc1dd5fe72cb1"
+    },
+    {
+        "symbol": "msw_usdc_usdt",
+        "contract_address": "0x01ea237607b7d9d2e9997aa373795929807552503683e35d8739f4dc46652de1"
+    },
+    {
+        "symbol": "msw_dai_eth",
+        "contract_address": "0x07c662b10f409d7a0a69c8da79b397fd91187ca5f6230ed30effef2dceddc5b3"
+    },
+    {
+        "symbol": "msw_eth_usdc",
+        "contract_address": "0x022b05f9396d2c48183f6deaf138a57522bcc8b35b67dee919f76403d1783136"
+    },
+    {
+        "symbol": "msw_eth_usdt",
+        "contract_address": "0x041f9a1e9a4d924273f5a5c0c138d52d66d2e6a8bee17412c6b0f48fe059ae04"
+    }
 ]

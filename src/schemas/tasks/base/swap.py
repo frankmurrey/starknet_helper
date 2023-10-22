4d055c47cf37f439f67e7fd25ac2956156d02333
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-22 16:35:51 +0500

    ADD random_y coin field

--- src/schemas/tasks/base/swap.py
+++ src/schemas/tasks/base/swap.py
@@ -1,11 +1,13 @@
-from typing import Union
+import random
 
 from pydantic import validator
 
 from src.schemas import validation_mixins
 from src.schemas.tasks.base import TaskBase
 from src.exceptions import AppValidationError
+from contracts.tokens.main import Tokens
 from utils import validation
+from src import enums
 
 
 class SwapTaskBase(
@@ -13,6 +15,8 @@ class SwapTaskBase(
     validation_mixins.SlippageValidationMixin,
     validation_mixins.MinMaxAmountOutValidationMixin,
 ):
+    random_y_coin: bool = False
+
     coin_x: str
     coin_y: str
 
@@ -28,15 +32,32 @@ class SwapTaskBase(
 
     slippage: float = 0.5
 
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        if self.random_y_coin:
+            if self.module_name == enums.ModuleName.RANDOM:
+                self.coin_y = enums.MiscTypes.RANDOM
+            else:
+                protocol_coins_obj = Tokens().get_tokens_by_protocol(self.module_name)
+                protocol_coins = [coin.symbol for coin in protocol_coins_obj]
+                protocol_coins.remove(self.coin_x.lower())
+                coin_y = random.choice(protocol_coins)
+                self.coin_y = coin_y.upper()
+
     @property
     def action_info(self):
-        if self.reverse_action:
-            return f"{self.coin_x.upper()} <-> {self.coin_y.upper()}"
-        else:
-            return f"{self.coin_x.upper()} -> {self.coin_y.upper()}"
+        coin_y = "Rand" if self.random_y_coin else self.coin_y.upper()
+        symbol = "->" if not self.reverse_action else "<->"
+
+        info = f"{self.coin_x.upper()} {symbol} {coin_y}"
+
+        return info
 
     @validator("coin_y", pre=True)
     def validate_coin_to_receive_pre(cls, value, values):
+        if values["random_y_coin"]:
+            return value
 
         if value == values["coin_x"]:
             raise AppValidationError("Coin to receive cannot be the same as Coin to swap")

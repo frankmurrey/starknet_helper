8154b56dc385b6b075c8180665442d79a5cb7645
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-22 16:40:05 +0500

    ADD random liquidity module

--- modules/random_module/liquidity.py
+++ modules/random_module/liquidity.py
@@ -3,16 +3,49 @@ from modules.base import LiquidityModuleBase
 from src.schemas.action_models import ModuleExecutionResult
 from src.schemas.tasks import AddLiquidityTaskBase
 from src.schemas import tasks
+from contracts.tokens.main import Tokens
 
 
 LIQUIDITY_TASKS = [
-    tasks.LiquidSwapAddLiquidityTask,
-    tasks.ThalaAddLiquidityTask,
+    tasks.SithSwapAddLiquidityTask,
+    tasks.JediSwapAddLiquidityTask,
+    tasks.MySwapAddLiquidityTask
 ]
 
 
 class RandomAddLiquidity(LiquidityModuleBase):
-    def try_send_txn(
+    def __init__(
+            self,
+            account,
+            task: AddLiquidityTaskBase,
+    ):
+        super().__init__(
+            account=account,
+            task=task,
+        )
+        random_task_class = random.choice(LIQUIDITY_TASKS)
+        task_dict = self.task.dict(exclude={"module_name",
+                                            "module_type",
+                                            "module"})
+        random_task: AddLiquidityTaskBase = random_task_class(**task_dict)
+        if random_task.random_y_coin:
+            protocol_coins_obj = Tokens().get_tokens_by_protocol(random_task.module_name)
+            protocol_coins = [coin.symbol for coin in protocol_coins_obj]
+            protocol_coins.remove(random_task.coin_x.lower())
+            random_task.coin_y = random.choice(protocol_coins)
+
+            self.task = random_task
+
+            self.coin_x = self.tokens.get_by_name(self.task.coin_x)
+            self.coin_y = self.tokens.get_by_name(self.task.coin_y)
+
+            self.initial_balance_x_wei = None
+            self.initial_balance_y_wei = None
+
+            self.token_x_decimals = None
+            self.token_y_decimals = None
+
+    async def try_send_txn(
             self,
             retries: int = 1,
     ) -> ModuleExecutionResult:
@@ -21,16 +54,10 @@ class RandomAddLiquidity(LiquidityModuleBase):
         :param retries:
         :return:
         """
-        random_task_class = random.choice(LIQUIDITY_TASKS)
-        task_dict = self.task.dict(exclude={"module_name",
-                                            "module_type",
-                                            "module"})
-        random_task: AddLiquidityTaskBase = random_task_class(**task_dict)
+        await self.set_fetched_tokens_data()
 
-        module = random_task.module(
+        module = self.task.module(
             account=self.account,
-            task=random_task,
-            base_url=self.base_url,
-            proxies=self.proxies,
+            task=self.task
         )
-        return module.try_send_txn(retries=retries)
+        return await module.try_send_txn(retries=retries)

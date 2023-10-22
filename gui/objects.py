d5f662e4753d751d4b19951b1bd16a90a8cebbbc
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-22 16:45:01 +0500

    ADD ComboWithRandomCheckBox

--- gui/objects.py
+++ gui/objects.py
@@ -1,4 +1,5 @@
 import tkinter
+import random
 from typing import Optional, Callable
 
 import customtkinter
@@ -121,3 +122,55 @@ class CTkCustomTextBox(customtkinter.CTkTextbox):
         self.configure(height=height)
         self.grid(**grid)
         self.insert("1.0", text)
+
+
+class ComboWithRandomCheckBox:
+    def __init__(
+            self,
+            master,
+            grid: dict,
+            options: list,
+            text: str = "Random",
+            combo_command: Optional[Callable] = lambda _: None,
+    ):
+        self.options = options
+
+        self.combobox = customtkinter.CTkComboBox(
+            master=master,
+            values=options,
+            width=130,
+            command=combo_command
+        )
+        self.combobox.grid(**grid)
+
+        self.random_checkbox = customtkinter.CTkCheckBox(
+            master=master,
+            text=text,
+            checkbox_width=18,
+            checkbox_height=18,
+            onvalue=True,
+            offvalue=False,
+            command=self.random_checkbox_event,
+        )
+        self.random_checkbox.grid(row=grid["row"] + 1, column=grid["column"], padx=20, pady=5, sticky="w")
+
+    def random_checkbox_event(self):
+        if self.random_checkbox.get():
+            self.combobox.configure(
+                state="disabled",
+                fg_color='#3f3f3f',
+            )
+        else:
+            self.combobox.configure(
+                state="normal",
+                fg_color='#343638',
+            )
+
+    def get_value(self):
+        if self.random_checkbox.get():
+            return random.choice(self.options)
+        return self.combobox.get()
+
+    def get_checkbox_value(self):
+        return self.random_checkbox.get()
+

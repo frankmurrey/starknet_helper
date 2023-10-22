8ed02a6f259aa603895757d2bc6834a5a3d1dd02
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-22 16:45:32 +0500

    CLR

--- gui/modules/swap.py
+++ gui/modules/swap.py
@@ -8,9 +8,20 @@ from tkinter import Variable
 from loguru import logger
 
 from gui.modules.txn_settings_frame import TxnSettingFrame
+from gui.objects import ComboWithRandomCheckBox
 from contracts.tokens.main import Tokens
 from src import enums
 from src.schemas import tasks
+
+
+SWAP_PROTOCOLS_DATA = {
+    enums.ModuleName.JEDI_SWAP: tasks.JediSwapTask,
+    enums.ModuleName.SITHSWAP: tasks.SithSwapTask,
+    enums.ModuleName.MY_SWAP: tasks.MySwapTask,
+    enums.ModuleName.AVNU: tasks.AvnuSwapTask,
+    enums.ModuleName.K10SWAP: tasks.K10SwapTask,
+    enums.ModuleName.FIBROUS: tasks.FibrousSwapTask,
+}
 
 
 class SwapTab:
@@ -45,24 +56,12 @@ class SwapTab:
         )
 
     def get_config_schema(self) -> Union[Callable, None]:
-        swap_protocol = self.swap_frame.protocol_combo.get().lower()
-        if swap_protocol == enums.ModuleName.JEDI_SWAP:
-            return tasks.JediSwapTask
-
-        elif swap_protocol == enums.ModuleName.SITHSWAP:
-            return tasks.SithSwapTask
-
-        elif swap_protocol == enums.ModuleName.MY_SWAP:
-            return tasks.MySwapTask
-
-        elif swap_protocol == enums.ModuleName.AVNU:
-            return tasks.AvnuSwapTask
-
-        elif swap_protocol == enums.ModuleName.K10SWAP:
-            return tasks.K10SwapTask
+        swap_protocol = self.swap_frame.protocol_combo.get_value().lower()
+        if self.swap_frame.protocol_combo.get_checkbox_value():
+            return tasks.RandomSwapTask
 
         else:
-            return None
+            return SWAP_PROTOCOLS_DATA.get(swap_protocol, None)
 
     def build_config_data(self):
         config_schema = self.get_config_schema()
@@ -73,7 +72,8 @@ class SwapTab:
         try:
             config_data: tasks.SwapTaskBase = config_schema(
                 coin_x=self.swap_frame.coin_to_swap_combo.get(),
-                coin_y=self.swap_frame.coin_to_receive_combo.get(),
+                coin_y=self.swap_frame.coin_to_receive_combo.combobox.get(),
+                random_y_coin=self.swap_frame.coin_to_receive_combo.get_checkbox_value(),
                 min_amount_out=self.swap_frame.min_amount_entry.get(),
                 max_amount_out=self.swap_frame.max_amount_entry.get(),
                 use_all_balance=self.swap_frame.use_all_balance_checkbox.get(),
@@ -108,18 +108,18 @@ class SwapFrame(customtkinter.CTkFrame):
         self.protocol_label = customtkinter.CTkLabel(self.frame, text="Protocol:")
         self.protocol_label.grid(row=0, column=0, padx=20, pady=(10, 0), sticky="w")
 
-        self.protocol_combo = customtkinter.CTkComboBox(
+        self.protocol_combo = ComboWithRandomCheckBox(
             self.frame,
-            values=self.protocol_options,
-            width=120,
-            command=self.protocol_change_event,
+            grid={"row": 1, "column": 0, "padx": 20, "pady": 0, "sticky": "w"},
+            options=self.protocol_options,
+            combo_command=self.protocol_change_event,
+            text="Random protocol",
         )
-        self.protocol_combo.grid(row=1, column=0, padx=20, pady=0, sticky="w")
 
         self.coin_to_swap_label = customtkinter.CTkLabel(
             self.frame, text="Coin to swap:"
         )
-        self.coin_to_swap_label.grid(row=2, column=0, padx=20, pady=(10, 0), sticky="w")
+        self.coin_to_swap_label.grid(row=3, column=0, padx=20, pady=(10, 0), sticky="w")
 
         self.coin_to_swap_combo = customtkinter.CTkComboBox(
             self.frame,
@@ -127,19 +127,21 @@ class SwapFrame(customtkinter.CTkFrame):
             width=120,
             command=self.update_coin_options,
         )
-        self.coin_to_swap_combo.grid(row=3, column=0, padx=20, pady=0, sticky="w")
+        self.coin_to_swap_combo.grid(row=4, column=0, padx=20, pady=0, sticky="w")
 
         self.coin_to_receive_label = customtkinter.CTkLabel(
             self.frame, text="Coin to receive:"
         )
         self.coin_to_receive_label.grid(
-            row=2, column=1, padx=20, pady=(10, 0), sticky="w"
+            row=3, column=1, padx=20, pady=(10, 0), sticky="w"
         )
 
-        self.coin_to_receive_combo = customtkinter.CTkComboBox(
-            self.frame, width=120, values=self.coin_to_receive_options
+        self.coin_to_receive_combo = ComboWithRandomCheckBox(
+            self.frame,
+            grid={"row": 4, "column": 1, "padx": 20, "pady": 0, "sticky": "w"},
+            options=self.coin_to_receive_options,
+            text="Random dst coin",
         )
-        self.coin_to_receive_combo.grid(row=3, column=1, padx=20, pady=0, sticky="w")
 
         self.reverse_action_checkbox = customtkinter.CTkCheckBox(
             self.frame,
@@ -150,20 +152,20 @@ class SwapFrame(customtkinter.CTkFrame):
             checkbox_height=18,
         )
         self.reverse_action_checkbox.grid(
-            row=4, column=0, padx=20, pady=(10, 0), sticky="w"
+            row=5, column=0, padx=20, pady=(10, 0), sticky="w"
         )
 
         self.min_amount_label = customtkinter.CTkLabel(self.frame, text="Min amount:")
-        self.min_amount_label.grid(row=5, column=0, padx=20, pady=(10, 0), sticky="w")
+        self.min_amount_label.grid(row=6, column=0, padx=20, pady=(10, 0), sticky="w")
 
         self.min_amount_entry = customtkinter.CTkEntry(self.frame, width=120)
-        self.min_amount_entry.grid(row=6, column=0, padx=20, pady=0, sticky="w")
+        self.min_amount_entry.grid(row=7, column=0, padx=20, pady=0, sticky="w")
 
         self.max_amount_label = customtkinter.CTkLabel(self.frame, text="Max amount:")
-        self.max_amount_label.grid(row=5, column=1, padx=20, pady=(10, 0), sticky="w")
+        self.max_amount_label.grid(row=6, column=1, padx=20, pady=(10, 0), sticky="w")
 
         self.max_amount_entry = customtkinter.CTkEntry(self.frame, width=120)
-        self.max_amount_entry.grid(row=6, column=1, padx=20, pady=0, sticky="w")
+        self.max_amount_entry.grid(row=7, column=1, padx=20, pady=0, sticky="w")
 
         self.use_all_balance_checkbox = customtkinter.CTkCheckBox(
             self.frame,
@@ -175,7 +177,7 @@ class SwapFrame(customtkinter.CTkFrame):
             command=self.use_all_balance_checkbox_event,
         )
         self.use_all_balance_checkbox.grid(
-            row=7, column=0, padx=20, pady=(10, 0), sticky="w"
+            row=8, column=0, padx=20, pady=(10, 0), sticky="w"
         )
 
         self.send_percent_balance_checkbox = customtkinter.CTkCheckBox(
@@ -187,29 +189,29 @@ class SwapFrame(customtkinter.CTkFrame):
             checkbox_height=18,
         )
         self.send_percent_balance_checkbox.grid(
-            row=8, column=0, padx=20, pady=(5, 0), sticky="w"
+            row=9, column=0, padx=20, pady=(5, 0), sticky="w"
         )
 
         self.slippage_label = customtkinter.CTkLabel(self.frame, text="Slippage (%):")
-        self.slippage_label.grid(row=9, column=1, padx=20, pady=(10, 0), sticky="w")
+        self.slippage_label.grid(row=10, column=1, padx=20, pady=(10, 0), sticky="w")
 
         self.slippage_entry = customtkinter.CTkEntry(
             self.frame, width=70, textvariable=Variable(value=2)
         )
-        self.slippage_entry.grid(row=10, column=1, padx=20, pady=0, sticky="w")
+        self.slippage_entry.grid(row=11, column=1, padx=20, pady=0, sticky="w")
 
         self.max_price_difference_percent_label = customtkinter.CTkLabel(
             self.frame, text="Max price difference (%):"
         )
         self.max_price_difference_percent_label.grid(
-            row=9, column=0, padx=20, pady=(10, 0), sticky="w"
+            row=10, column=0, padx=20, pady=(10, 0), sticky="w"
         )
 
         self.max_price_difference_percent_entry = customtkinter.CTkEntry(
             self.frame, width=120, textvariable=Variable(value=2)
         )
         self.max_price_difference_percent_entry.grid(
-            row=10, column=0, padx=20, pady=0, sticky="w"
+            row=11, column=0, padx=20, pady=0, sticky="w"
         )
 
         self.compare_with_cg_price_checkbox = customtkinter.CTkCheckBox(
@@ -222,24 +224,18 @@ class SwapFrame(customtkinter.CTkFrame):
             command=self.compare_with_cg_price_checkbox_event,
         )
         self.compare_with_cg_price_checkbox.grid(
-            row=11, column=0, padx=20, pady=10, sticky="w"
+            row=12, column=0, padx=20, pady=10, sticky="w"
         )
         self.compare_with_cg_price_checkbox.select()
 
     @property
     def protocol_options(self) -> list:
-        return [
-            enums.ModuleName.SITHSWAP.upper(),
-            enums.ModuleName.MY_SWAP.upper(),
-            enums.ModuleName.AVNU.upper(),
-            enums.ModuleName.K10SWAP.upper(),
-            enums.ModuleName.JEDI_SWAP.upper(),
-        ]
+        return [name.upper() for name in SWAP_PROTOCOLS_DATA.keys()]
 
     @property
     def protocol_coin_options(self) -> list:
         tokens = Tokens()
-        protocol = self.protocol_combo.get()
+        protocol = self.protocol_combo.get_value()
 
         return [
             token.symbol.upper() for token in tokens.get_tokens_by_protocol(protocol)
@@ -265,8 +261,8 @@ class SwapFrame(customtkinter.CTkFrame):
         self.coin_to_swap_combo.configure(values=coin_to_swap_options)
 
         coin_to_receive_options = self.coin_to_receive_options
-        self.coin_to_receive_combo.configure(values=coin_to_receive_options)
-        self.coin_to_receive_combo.set(coin_to_receive_options[1])
+        self.coin_to_receive_combo.combobox.configure(values=coin_to_receive_options)
+        self.coin_to_receive_combo.combobox.set(coin_to_receive_options[1])
 
     def protocol_change_event(self, protocol=None):
         coin_to_swap_options = self.coin_to_swap_options
@@ -274,8 +270,8 @@ class SwapFrame(customtkinter.CTkFrame):
         self.coin_to_swap_combo.set(coin_to_swap_options[1])
 
         coin_to_receive_options = self.coin_to_receive_options
-        self.coin_to_receive_combo.configure(values=coin_to_receive_options)
-        self.coin_to_receive_combo.set(coin_to_receive_options[1])
+        self.coin_to_receive_combo.combobox.configure(values=coin_to_receive_options)
+        self.coin_to_receive_combo.combobox.set(coin_to_receive_options[1])
 
     def use_all_balance_checkbox_event(self):
         if self.use_all_balance_checkbox.get():

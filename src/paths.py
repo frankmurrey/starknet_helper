68bc3ea9f86fb3089f08d180ca58aeba97f03645
preposition17 <yupich17@gmail.com>
2023-11-03 21:09:08 +0500

    FIX frank bil prav

--- src/paths.py
+++ src/paths.py
@@ -79,6 +79,7 @@ class StarkBridgeDir:
 
 
 class TempFiles:
-    TOKENS_JSON_FILE = os.path.join(CONTRACTS_DIR, "tokens.json")
-    APP_CONFIG_JSON_FILE = os.path.join(MAIN_DIR, "app_config.json")
-    LOGS_DIR = os.path.join(MAIN_DIR, "logs")
+    def __init__(self):
+        self.TOKENS_JSON_FILE = os.path.join(CONTRACTS_DIR, "tokens.json")
+        self.APP_CONFIG_JSON_FILE = os.path.join(MAIN_DIR, "app_config.json")
+        self.LOGS_DIR = os.path.join(MAIN_DIR, "logs")

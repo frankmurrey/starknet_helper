437ce4befca74a8bbc1bf9f6d13fd3adf995cf3d
preposition17 <yupich17@gmail.com>
2023-11-03 20:13:59 +0500

    ADD get missed reqs function

--- utils/system.py
+++ utils/system.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 import functools
+from typing import Tuple, List
+
+from src import paths
 
 
 @functools.cache
@@ -27,3 +30,35 @@ def mingw_installed() -> bool:
 
     except FileNotFoundError:
         return False
+
+
+@functools.cache
+def get_missed_requirements() -> List[str]:
+    """
+    Get requirements that are not installed.
+    Returns: list of missing requirements
+    """
+
+    with open(os.path.join(paths.MAIN_DIR, "req.txt"), "r", encoding="utf-8") as f:
+        requirements = f.readlines()
+        requirements = [
+            req.split("==")[0].strip().lower()
+            for req in requirements
+        ]
+
+    exec_result = (
+        subprocess.check_output(["pip", "freeze"])
+        .decode("utf-8")
+        .lower()
+    )
+    installed_requirements = [
+        req.split("==")[0].strip().lower()
+        for req in exec_result.splitlines()
+    ]
+
+    missed_requirements = []
+    for req in requirements:
+        if req not in installed_requirements:
+            missed_requirements.append(req)
+
+    return missed_requirements

58e6e490e0b8107f41b7c3dadfd91a8a890f3f9b
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-11-03 21:23:41 +0500

    ADD float spin box

--- gui/objects.py
+++ gui/objects.py
@@ -7,6 +7,77 @@ import customtkinter
 from utils import gui as gui_utils
 
 
+class FloatSpinbox(customtkinter.CTkFrame):
+    def __init__(
+            self,
+            *args,
+            start_index: int = 1,
+            max_value: Union[int] = 100,
+            width: int = 100,
+            height: int = 32,
+            step_size: Union[int, float] = 1,
+            command: Callable = None,
+            **kwargs
+    ):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.step_size = int(step_size)
+        self.max_value = int(max_value)
+        self.command = command
+
+        self.configure(fg_color=("gray78", "gray21"))
+
+        self.grid_columnconfigure((0, 2), weight=0)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.subtract_button = customtkinter.CTkButton(self, text="-", width=height-6, height=height-6,
+                                                       command=self.subtract_button_callback)
+        self.subtract_button.grid(row=0, column=0, padx=(3, 0), pady=3)
+
+        self.entry = customtkinter.CTkEntry(self, width=width-(2*height), height=height-6, border_width=0, fg_color="gray16")
+        self.entry.grid(row=0, column=1, columnspan=1, padx=3, pady=3, sticky="ew")
+
+        self.add_button = customtkinter.CTkButton(self, text="+", width=height-6, height=height-6,
+                                                  command=self.add_button_callback)
+        self.add_button.grid(row=0, column=2, padx=(0, 3), pady=3)
+
+        self.entry.insert(0, start_index)
+
+    def add_button_callback(self):
+        if self.command is not None:
+            self.command()
+        try:
+            value = int(self.entry.get()) + self.step_size
+            if value > self.max_value:
+                value = self.max_value
+            self.entry.delete(0, "end")
+            self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def subtract_button_callback(self):
+        if self.command is not None:
+            self.command()
+        try:
+            value = int(self.entry.get()) - self.step_size
+            if value < 1:
+                value = 1
+            self.entry.delete(0, "end")
+            self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def get(self) -> Union[float, None]:
+        try:
+            return float(self.entry.get())
+        except ValueError:
+            return None
+
+    def set(self, value: float):
+        self.entry.delete(0, "end")
+        self.entry.insert(0, str(int(value)))
+
+
 class CTkEntryWithLabel(customtkinter.CTkFrame):
     """
     Entry with a label

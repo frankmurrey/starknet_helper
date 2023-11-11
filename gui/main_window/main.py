b8e3eece3209f618e1bb996c6a28284f085fc096
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-11-11 19:48:18 +0500

    ADD tray icon

--- gui/main_window/main.py
+++ gui/main_window/main.py
@@ -4,6 +4,9 @@ import customtkinter
 
 from src.tasks_executor import tasks_executor
 from src.logger import configure_logger
+from PIL import Image, ImageDraw
+import pystray
+from pystray import MenuItem as item
 
 from gui.main_window.frames import SidebarFrame
 from gui.wallet_right_window.right_frame import RightFrame
@@ -41,16 +44,47 @@ class MainWindow(customtkinter.CTk):
 
         self.right_frame = RightFrame(self)
 
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.protocol("WM_DELETE_WINDOW", self.hide_window)
+
+    def create_image(self, width, height, color1, color2):
+        image = Image.new('RGB', (width, height), color1)
+        dc = ImageDraw.Draw(image)
+        dc.rectangle(
+            [width // 2, 0, width, height // 2],
+            fill=color2
+        )
+        dc.rectangle(
+            [0, height // 2, width // 2, height],
+            fill=color2
+        )
+
+        return image
+
+    def init_tray_icon(self):
+        icon_image = self.create_image(64, 64, 'black', 'red')
+        menu = (item('Show', self.show_window), item('Quit', self.quit_window))
+        self.tray_icon = pystray.Icon("tray_icon", icon_image, "StarkNet Helper", menu)
+        self.tray_icon.run_detached()
+
+    def hide_window(self):
+        self.withdraw()
+
+    def show_window(self, icon, item):
+        self.deiconify()
+
+    def quit_window(self, icon, item):
+        icon.visible = False
+        icon.stop()
+        self.on_closing()
 
     def on_start(self):
         configure_logger()
         Templates().create_not_found_temp_files()
         print_logo()
 
+        self.init_tray_icon()
+
     def on_closing(self):
         tasks_executor.stop()
-        self.destroy()
         self.quit()
 
-        sys.exit()

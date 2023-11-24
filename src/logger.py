4e92aca11f57ed63a8ca2b352ab5ebe7d05756a2
preposition17 <yupich17@gmail.com>
2023-11-24 19:48:37 +0500

    CLR

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,12 @@ import config
 
 def configure_logger():
     logger.remove()
-    logger.add(stdout,
-               format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{line: <3}</cyan>"
-                      " - <level>{message}</level>", level=config.LOGGING_LEVEL)
-    pass
+    logger.add(
+        stdout,
+        format=(
+            "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{line: <3}</cyan> - <level>{message}</level>"
+        ),
+        level=config.LOGGING_LEVEL
+    )

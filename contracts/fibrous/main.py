16430ee482871d5360b8ab4b67988d1a7d5add4b
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-11-24 17:55:11 +0500

    UPD contract address

--- contracts/fibrous/main.py
+++ contracts/fibrous/main.py
@@ -4,5 +4,5 @@ from utils.file_manager import FileManager
 
 class FibrousContracts:
     def __init__(self):
-        self.router_address = '0x03201e8057a781dca378564b9d3bbe9b5b7617fac4ad9d9deaa1024cf63f877e'
+        self.router_address = '0x00f6f4CF62E3C010E0aC2451cC7807b5eEc19a40b0FaaCd00CCA3914280FDf5a'
         self.router_abi = FileManager.read_abi_from_file(FibrousDir.ROUTER_ABI_FILE)

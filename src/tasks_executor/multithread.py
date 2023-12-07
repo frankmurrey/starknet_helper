f8b009acaf0548b10cf7c2099c3713d0571c1ea3
preposition17 <yupich17@gmail.com>
2023-12-07 23:55:40 +0500

    UPD task executor

--- src/tasks_executor/multithread.py
+++ src/tasks_executor/multithread.py
@@ -1,6 +1,7 @@
 import time
 import asyncio
 import threading as th
+from concurrent.futures import ThreadPoolExecutor
 from typing import Optional, List
 
 from loguru import logger
@@ -10,7 +11,10 @@ from src.schemas.tasks.base.base import TaskBase
 from src.schemas.wallet_data import WalletData
 from src.storage import Storage
 from src.tasks_executor.base import TaskExecutorBase
-from src.logger import configure_logger
+from src.repr.repr_manager import ReprManager
+from src.repr.event_manager import repr_event_manager
+from src.logger import configure_threaded_task_executor_logger
+from utils.repr import message as repr_message_utils
 
 import config
 
@@ -39,6 +43,7 @@ class TaskExecutorMultiThread(TaskExecutorBase):
         super().__init__()
 
         self.lock: Optional[th.Lock] = None
+        self.log_event_manager = repr_event_manager
 
     def _start_processing(
             self,
@@ -50,32 +55,19 @@ class TaskExecutorMultiThread(TaskExecutorBase):
         """
 
         Storage().update_app_config(config=AppConfigSchema(**self._app_config_dict))
-        configure_logger()
+        configure_threaded_task_executor_logger(self.log_event_manager.queue)
         self.lock = th.Lock()
 
-        wallet_threads = []
-        for wallet_index, wallet in enumerate(wallets):
-            clear_threads(wallet_threads, max_threads_num=config.DEFAULT_WALLETS_THREADS_NUM)
-            is_last_wallet = wallet_index == len(wallets) - 1
-            loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor(max_workers=config.DEFAULT_WALLETS_THREADS_NUM) as executor:
+            futures = []
+            for wallet_index, wallet in enumerate(wallets):
+                loop = asyncio.get_event_loop()
+                future = executor.submit(self.process_wallet, wallet, tasks, loop)
+                futures.append(future)
 
-            thread = th.Thread(
-                target=self.process_wallet,
-                args=(
-                    wallet,
-                    tasks,
+                if wallet_index != len(wallets) - 1:
+                    time.sleep(config.DEFAULT_DELAY_SEC)
 
-                    loop,
-                )
-            )
-
-            if not is_last_wallet:
-                time.sleep(config.DEFAULT_DELAY_SEC)
-
-            thread.start()
-            wallet_threads.append(thread)
-
-        clear_threads(wallet_threads)
         logger.success("All wallets processed")
 
     def process_wallet(
@@ -94,10 +86,8 @@ class TaskExecutorMultiThread(TaskExecutorBase):
         """
 
         asyncio.set_event_loop(loop)
-        self.wait_for_unlock()
 
-        with self.lock:
-            self.event_manager.set_wallet_started(wallet)
+        self.event_manager.set_wallet_started(wallet)
 
         self._process_wallet(wallet=wallet, tasks=tasks)
         self.event_manager.set_wallet_completed(wallet)
@@ -108,11 +98,18 @@ class TaskExecutorMultiThread(TaskExecutorBase):
             wallet: "WalletData",
     ):
         self.event_manager.set_task_started(task, wallet)
-        with self.lock:
-            task_result = self._process_task(
+
+        ReprManager.push_repr_item_to_thread_items(
+            repr_message_utils.task_start_message(
                 task=task,
                 wallet=wallet,
-            )
+            ),
+            thread=th.get_ident(),
+        )
+        task_result = self._process_task(
+            task=task,
+            wallet=wallet,
+        )
         self.event_manager.set_task_completed(task, wallet)
 
         return task_result
